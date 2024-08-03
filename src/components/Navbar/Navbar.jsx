86b6decb124f1633a1786069b0e5463f380c6a96
harsh choubey <choubeyh886@gmail.com>
2024-08-03 17:48:33 +0530

    title changed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css"
 const Navbar = () => {
     return (
         <div className="Navbar-root">
-                QuadB Tech Intership Task
+                Movie.IO
         </div>
     )
 }
